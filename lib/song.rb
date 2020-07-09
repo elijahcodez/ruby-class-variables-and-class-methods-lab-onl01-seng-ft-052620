e86818e5f36452b33e694cc37f002659815520c1
Elijah Kirschen <ekirschen584@gmail.com>
2020-07-09 05:31:34 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,49 +1,53 @@
 class Song 
-  @@count = 0
-  
-  def self.count 
-    @@count
-  end 
+ 
+attr_accessor :name, :artist, :genre
 
-  @@genres = []
+@@count = 0
+@@artists = []
+@@genres = []
+def initialize(name, artist, genre)
   
-  def self.genres
-    @@genres.uniq
-  end
-  
-  def self.genre_count
-    @@genres.inject(Hash.new(0)) { |total, i| total[i] += 1 ;total}
-  end 
-  
-  def self.genre_count
-    genre_count = {}
-    @@genres.each do |genre|
-      if genre_count[genre]
-        genre_count[genre] += 1 
-      else
-        genre_count[genre] = 1
-      end
-    end
-    genre_count
-  end
-  
-  @@artists = []
-  
-  def self.artist_count
-    @@artists.inject(Hash.new(0)) { |total, i| total[i] += 1 ;total}
-  end
-  
-  def self.artists
-    @@artists.uniq
-  end
-  
-  def initialize(name, artist, genre)
-    @name = name
-    @artist = artist
-    @@artists << artist
-    @genre = genre
-    @@genres << genre
-    @@count += 1
-  end
-  attr_accessor :name, :artist, :genre
+  @name = name
+ @artist = artist
+  @genre = genre
+  @@count +=1
+  @@artists << @artist
+ @@genres << @genre
+end
+
+def self.count 
+@@count
+end
+
+def self.artists
+  @@artists.uniq
 end 
+
+
+def self.genres
+  
+  @@genres.uniq
+end 
+
+def self.genre_count 
+genre_count = {}
+
+self.genres.each do |genre|
+genre_count[genre] = @@genres.count {|g| g == genre}
+
+end
+genre_count
+end 
+
+
+def self.artist_count
+artist_count = {}
+
+self.artists.each do |artist|
+artist_count[artist] = @@artists.count {|a| a == artist}
+
+end
+artist_count
+end 
+
+end
